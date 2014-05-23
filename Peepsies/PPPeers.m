a3c475a9de66ec2f0c6f22425f06cafc28c6d98c
Talus <taldar@eightt.us>
2014-05-23 12:26:43 -0600

    Damn you, autosaving!!!

--- Peepsies/PPPeers.m
+++ Peepsies/PPPeers.m
@@ -7,7 +7,50 @@
 //
 
 #import "PPPeers.h"
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
+
+
+@interface PPPeers ()
+
+@property (nonatomic) MCPeerID *ourID;
+@property (nonatomic) MCSession *ourPublishedSession;
+@property (nonatomic) NSMutableSet *sessionsWeBelongTo;
+@property (nonatomic) dispatch_queue_t eventQueue;
+@property (nonatomic) NSDictionary *posts;
+
+@end
+
 
 @implementation PPPeers
+
+static PPPeers *_sharedSingleton = nil;
++ (void)initialize {
+    _sharedSingleton = [[PPPeers alloc] init];
+}
+
+- (PPPeers *)peers {
+    return _sharedSingleton;
+}
+
+
+- (id)init {
+    if (!(self = [super init]))
+        return nil;
+    
+    NSString *displayName = [[NSUserDefaults standardUserDefaults] objectForKey:@"PPUserDisplayName"];
+    
+    _ourID = [[MCPeerID alloc] initWithDisplayName:displayName];
+    _ourPublishedSession = [[MCSession alloc] initWithPeer:_ourID];
+    _sessionsWeBelongTo = [NSMutableSet setWithCapacity:8];
+    _eventQueue = dispatch_queue_create("PPPeersEventQueue", DISPATCH_QUEUE_SERIAL);
+    
+    _posts = [NSMutableDictionary dictionaryWithCapacity:30];
+    
+    
+    return self;
+}
+
+
+
 
 @end
