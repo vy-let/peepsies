70af185809890bba11054a99a82d2b02556df3cb
Talus <taldar@eightt.us>
2014-05-23 12:25:35 -0600

    Added PPPeers

---
+++ Peepsies/PPPeers.h
@@ -0,0 +1,13 @@
+//
+//  PPPeers.h
+//  Peepsies
+//
+//  Created by Taldar Baddley on 2014-5-23.
+//  Copyright (c) 2014 Peepsies Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PPPeers : NSObject
+
+@end
---
+++ Peepsies/PPPeers.m
@@ -0,0 +1,13 @@
+//
+//  PPPeers.m
+//  Peepsies
+//
+//  Created by Taldar Baddley on 2014-5-23.
+//  Copyright (c) 2014 Peepsies Ltd. All rights reserved.
+//
+
+#import "PPPeers.h"
+
+@implementation PPPeers
+
+@end
